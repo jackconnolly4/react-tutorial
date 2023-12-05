f6f00a804ff0d55270bb2e6da9361cf5fc02b4db
Jack <jconnolly444@gmail.com>
2023-12-05 14:04:55 -0600

    added submit form

--- src/App.js
+++ src/App.js
@@ -1,27 +1,11 @@
 import React, { Component } from "react";
 import Table from "./Table";
 import ReactPlayer from "react-player";
+import Form from "./Form";
 
 class App extends Component {
   state = {
-    characters: [
-      {
-        name: "Charlie",
-        job: "Janitor",
-      },
-      {
-        name: "Mac",
-        job: "Bouncer",
-      },
-      {
-        name: "Dee",
-        job: "Aspring actress",
-      },
-      {
-        name: "Dennis",
-        job: "Bartender",
-      },
-    ],
+    characters: [],
   };
   removeCharacter = (index) => {
     const { characters } = this.state;
@@ -32,11 +16,15 @@ class App extends Component {
       }),
     });
   };
+  handleSubmit = (character) => {
+    this.setState({ characters: [...this.state.characters, character] });
+  };
   render() {
     const { characters } = this.state;
     return (
       <div className="container">
         <Table characterData={characters} removeCharacter={this.removeCharacter} />
+        <Form handleSubmit={this.handleSubmit} />
         <ReactPlayer className="react-player" url="https://www.youtube.com/watch?v=0cVlTeIATBs" />
       </div>
     );
